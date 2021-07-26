5a9142a15412cb62dff21f204422c2a9051a6175
sewonK <cutecat1128@naver.com>
2021-07-26 21:58:24 +0900

    Solve Hash problem

--- Python/해시_베스트앨범.py
+++ Python/해시_베스트앨범.py
@@ -1,6 +1,6 @@
 from collections import defaultdict
-genres = ["classic", "pop", "classic", "classic", "pop"]
-plays = [500, 600, 150, 800, 2500]
+genres = ["classic", "pop", "classic", "classic", "classic", "pop"]
+plays = [500, 600, 150, 800, 800, 2500]
 
 def solution(genres, plays):
     answer = []
@@ -14,7 +14,9 @@ def solution(genres, plays):
         genre_play[genres[i]] += [[i, plays[i]]]
     
     for genre in genre_rank:
+        # (x[1], x[0]) 아니고 x[1] 인 이유 -> 소팅 시 자동으로 key 값이 작은 순서대로 ?
         result = sorted(genre_play[genre], key = lambda x: (x[1]), reverse=True)
+        print(result)
         if len(result) > 1:
             answer.append(result[0][0])
             answer.append(result[1][0])
