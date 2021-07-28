4179cefcf3894ed323d5fa4c55eee11c66416a77
sewonK <cutecat1128@naver.com>
2021-07-28 20:04:56 +0900

    solve queue problem

--- Python/스택+큐/3.다리를 지나는 트럭.py
+++ Python/스택+큐/3.다리를 지나는 트럭.py
@@ -4,13 +4,17 @@ weight = 100
 truck_weights = [10,10,10,10,10,10,10,10,10,10]
 
 def solution(bridge_length, weight, truck_weights):
+    #초기화
     answer = 0
     trucks_in_bridge = deque([])
-    trucks_in_bridge.append([0, truck_weights[0]])
     waiting_truck = deque([idx, truck_weight] for idx, truck_weight in enumerate(truck_weights))
+    time = [0 for i in range(10001)]
+    
+    #1번 트럭 다리에 넣어주는 로직
+    trucks_in_bridge.append([0, truck_weights[0]])
     waiting_truck.popleft()
     total = truck_weights[0]
-    time = [0 for i in range(10001)]
+    
     while trucks_in_bridge:
         cur = trucks_in_bridge[0]
         if time[cur[0]] >= bridge_length: #다리 다 건넘
