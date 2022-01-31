a91290a54c4761cd7281c7b07f490ab447320810
sewonK <cutecat1128@naver.com>
2022-02-01 02:09:16 +0900

    [2018 카카오 블라인드] 방금 그곡 문제풀이 수정

--- Python/2018카카오블라인드/방금그곡.py
+++ Python/2018카카오블라인드/방금그곡.py
@@ -13,36 +13,27 @@ def add_zero(music):
         idx += 1
     return result          
 
+def get_play_time(start, end):
+    start_hh, start_mm = map(int, start.split(":"))
+    end_hh, end_mm = map(int, end.split(":"))
+    return (end_hh - start_hh) * 60 + end_mm - start_mm
+        
+def get_entire_music(music, play_time):
+    return music*((play_time*2) // len(music)) + music[:(play_time*2) % len(music)]
+
 def solution(m, musicinfos):
     answer = '(None)'
-    max_play_time = -1
-    
+    max_play_time = -1    
     m = add_zero(m)
     for musicinfo in musicinfos:
         start, end, title, music = musicinfo.split(",")
-        start_hh, start_mm = map(int, start.split(":"))
-        end_hh, end_mm = map(int, end.split(":"))
-        music = add_zero(music)
-        
-        play_time = end_mm - start_mm
-        if end_mm - start_mm < 0 :
-            end_hh -= 1
-            play_time += 60        
-        play_time += (end_hh - start_hh) * 60        
-        
-        if play_time*2 < len(music):
-            if m in music[:play_time*2]:
-                if max_play_time < play_time:
-                    max_play_time = play_time
-                    answer = title
-        else:
-            entire_music = music*((play_time*2)//len(music))
-            entire_music += music[:(play_time*2)%len(music)]
-            idx = entire_music.find(m)
-            if idx != -1:
-                if max_play_time < play_time:
-                    max_play_time = play_time
-                    answer = title
+        music = add_zero(music)        
+        play_time = get_play_time(start, end)
+        entire_music = get_entire_music(music, play_time)
+        idx = entire_music.find(m)
+        if (idx != -1) and (max_play_time < play_time):
+            max_play_time = play_time
+            answer = title
     return answer
 
 # print(solution("ABC",["12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"]))
